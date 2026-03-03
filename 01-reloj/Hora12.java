cbc377613613199e58798f965e26ce8df95a2b42
Pablo-Ventero <pabloventero07@gmail.com>
2026-03-03 10:01:32 +0100

    Create Hora12.java

---
+++ 01-reloj/Hora12.java
@@ -0,0 +1,20 @@
+package Reloj;
+
+public class Hora12 extends Hora{
+    private String tramoHorario;//am o pm
+
+    @Override
+    public void inc() {
+        super.inc(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void setHoras(int horas) {
+        super.setHoras(horas); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return "Hora12{" + "tramoHorario=" + tramoHorario + '}';
+    }
+}
